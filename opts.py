a68e3e7ffba6aa3030579329473e40b9a6aa8ea1
aiotgroup <96166485+aiotgroup@users.noreply.github.com>
2024-01-19 19:17:20 +0800

    Update opts.py

--- opts.py
+++ opts.py
@@ -2,7 +2,7 @@ import argparse
 
 def parse_opts():
     parser = argparse.ArgumentParser(description='XRFDataset')
-    parser.add_argument('--data_dir', type=str, default='/home/zhumengdie/XRFDataset/', help='Dataset Path')
+    parser.add_argument('--data_dir', type=str, default='path/to/load', help='Dataset Path')
     parser.add_argument('--class_num', type=int, default=55, help='The Number of Classes')
     parser.add_argument('--epoch', type=int, default=200, help='The Number of Epoch[default: 100]')
     parser.add_argument('--lr', type=float, default=0.001, help='learning rate [default: 0.001]')
